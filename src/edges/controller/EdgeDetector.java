641a5bf078e656f7498b0653c3cba4ba5aab9771
TaylorHogge <hoggetaylor@gmail.com>
2014-09-11 14:30:10 -0600

    Binary drawing works
    Detection and binary drawing works.
    Mapping values to a gradient image needs to be added.

--- src/edges/controller/EdgeDetector.java
+++ src/edges/controller/EdgeDetector.java
@@ -12,20 +12,27 @@ import javax.swing.JFileChooser;
 public class EdgeDetector
 {
 	private int threshold;
-	private boolean mapToGradient;
+	private boolean useGradient;
 	
 	private BufferedImage image;
 	private Component parentComponent;
 	
+	/**
+	 * Creates a new edge detector. threshold is set to 300 by default.
+	 * @param parentComponent the parent of the file selection dialog boxes.
+	 */
 	public EdgeDetector(Component parentComponent)
 	{
 		threshold = 300;
-		mapToGradient = false;
+		useGradient = false;
 		
 		image = null;
 		this.parentComponent = parentComponent;
 	}
 	
+	/**
+	 * User chooses an image, edge detection is performed, image is saved where the user wants.
+	 */
 	public void start()
 	{
 		chooseImage();
@@ -33,6 +40,9 @@ public class EdgeDetector
 		saveImage();
 	}
 	
+	/**
+	 * Opens a new file selection window so the user can select an image to perform on.
+	 */
 	private void chooseImage()
 	{
 		final JFileChooser fileChooser = new JFileChooser();
@@ -49,24 +59,55 @@ public class EdgeDetector
 		}
 	}
 	
+	/**
+	 * Changes the image based on the edges detected.
+	 */
 	private void performDetection()
 	{
-		for(int row = 0; row < image.getWidth(); row++)
+		int[][] values = new int[image.getWidth()][image.getHeight()];
+		
+		for(int x = 0; x < image.getWidth(); x++)
 		{
-			for(int col = 0; col < image.getHeight(); col++)
+			for(int y = 0; y < image.getHeight(); y++)
 			{
-				int rgb = image.getRGB(row,col);
-				Color c = new Color(rgb);
-				int r = c.getRed();
-				int b = c.getBlue();
-				int g = c.getGreen();
-				int grey = (r + b + g)/3;
-				Color c2 = 	new Color(grey, b, grey);
-				image.setRGB(row,col,c2.getRGB());
+				int grad = Math.abs(grad(x + 1, y) - grad(x - 1, y)) + Math.abs(grad(x, y + 1) - grad(x, y - 1));
+				values[x][y] = grad;
 			}
 		}
+		
+		if(!useGradient)
+		{
+			for(int x = 0; x < values.length; x++)
+			{
+				for(int y = 0; y < values[0].length; y++)
+				{
+					if(values[x][y] > threshold)
+					{
+						image.setRGB(x, y, Color.WHITE.getRGB());
+					}
+					else
+					{
+						image.setRGB(x, y, Color.BLACK.getRGB());
+					}
+				}
+			}
+		}
+		else
+		{
+			for(int x = 0; x < values.length; x++)
+			{
+				for(int y = 0; y < values[0].length; y++)
+				{
+					//code for gradient values goes here.
+				}
+			}
+		}
+		
 	}
 	
+	/**
+	 * Opens a file saving window to the user can specify where to save the edited image.
+	 */
 	private void saveImage()
 	{
 		final JFileChooser fileChooser = new JFileChooser();
@@ -74,7 +115,7 @@ public class EdgeDetector
 		File file = fileChooser.getSelectedFile();
 		try
 		{
-			ImageIO.write(image, "jpeg", file);
+			ImageIO.write(image, "png", file);
 		}
 		catch(IOException e)
 		{
@@ -82,14 +123,43 @@ public class EdgeDetector
 		}
 	}
 	
+	/**
+	 * gets the sum of all color channels found in the pixel at the given coordinates.
+	 * @param x x position of the pixel.
+	 * @param y y position of the pixel.
+	 * @return sum of all color channels combined.
+	 */
+	private int grad(int x, int y)
+	{
+		if(x > 0 && x < image.getWidth()) //x-axis in bounds
+		{
+			if(y > 0 && y < image.getHeight()) //y-axis in bounds
+			{
+				Color color = new Color(image.getRGB(x,y));
+				return (color.getRed() + color.getGreen() + color.getBlue());
+			}
+		}
+		
+		return 0; //out of bounds return 0
+	}
+	
 	//Setters
+	/**
+	 * Sets the threshold to be used when detecting edges.
+	 * @param threshold
+	 */
 	public void setThreshold(int threshold)
 	{
 		this.threshold = threshold;
 	}
 	
-	public void setMapToGradient(boolean mapToGradient)
+	/**
+	 * If false: a gray scale image will be produced based on the gradient values.
+	 * If true: a binary decision is made producing a black and white image.
+	 * @param mapToGradient
+	 */
+	public void setUseGradient(boolean useGradient)
 	{
-		this.mapToGradient = mapToGradient;
+		this.useGradient = useGradient;
 	}
 }
